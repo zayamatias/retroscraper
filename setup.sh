c07ac8bcf2cc962ca2abecb591f208aaebd997f1
zayamatias <32246591+zayamatias@users.noreply.github.com>
2022-11-08 15:42:43 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ source "/home/pi/.cargo/env"
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python3 /tmp/get-pip.py
 python3 -m pip install --user --upgrade pip wheel setuptools
-python3 -m pip install --user -r "$md_build/requirements.txt"
+python3 -m pip install --user -r "$1/requirements.txt"
